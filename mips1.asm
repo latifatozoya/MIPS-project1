82afd857ec199244a9213aa9fc4be385e6875dab
latifatozoya <latifat.ozoya@bison.howard.edu>
2018-10-18 16:44:07 -0400

    init

--- mips1.asm
+++ mips1.asm
@@ -1,7 +1,7 @@
 .data
 .text
 main:
-  li $v0, 11    # this is a system call code for  print character
+  li $v0, 11    # this is a syscall code for  print character
   la $a0, '@'   #this copies the @ character into register $a0
   syscall       #this is a call operating system that performs print operation
   li $v0, 1               #service 1 is used to print integer
